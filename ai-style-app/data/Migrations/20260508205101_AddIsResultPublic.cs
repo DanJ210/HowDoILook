9201c7db51d2bda40305b7516cbcae1221eef4d9
Daniel Jackson <danj210@hotmail.com>
2026-05-08 23:49:35 -0400

    Updating database

---
+++ ai-style-app/data/Migrations/20260508205101_AddIsResultPublic.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AiStyleApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsResultPublic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_result_public",
+                table: "style_items",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_result_public",
+                table: "style_items");
+        }
+    }
+}
